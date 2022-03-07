9774a835889e52671e6e668d59b892b16069b25d
Yaswell De la Rosa <yaswell_de_la_rosa@claro.com.do>
2022-03-07 12:18:20 -0400

    Setting up only test files

--- src/components/box/Actions.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Actions = ({digit, id, onclick}) => {
-  return (
-    <button id={id} onClick={onclick} value={digit} className='action'>{digit}</button>
-  )
-}
-
-export default Actions
--- src/components/box/Number.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Number = ({digit, onClick}) => {
-  return (
-    <button value={digit} onClick={onClick} className='digit'>{digit}</button>
-  )
-}
-
-export default Number
